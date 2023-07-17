b9e763d749c01d8b5578827a9a8d9a8bf716bd25
Adithyan2MS <adithyan2ms@gmail.com>
2023-07-17 23:28:33 +0530

    Install and Set Up Mongoose

--- myApp.js
+++ myApp.js
@@ -1,5 +1,7 @@
 require('dotenv').config();
+const mongoose = require('mongoose')
 
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 let Person;
 
@@ -51,7 +53,6 @@ const queryChain = (done) => {
   done(null /*, data*/);
 };
 
-console.log("hai")
 /** **Well Done !!**
 /* You completed these challenges, let's go celebrate !
  */
